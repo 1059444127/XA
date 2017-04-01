87c007e1bc2789bc52e7156a26ec39381bec3ef1
whhub <wanghui122004@me.com>
2017-04-01 14:32:11 +0800

    [IAS][DailyUpdate] 170331 CommonFeature.MiniBoot

---
+++ Trunk/IAS/CommonFeature/Core.Net/UIH.XR.MiniBoot/MiniApp.cs
@@ -0,0 +1,41 @@
+﻿/// ------------------------------------------------------------------------------
+/// <copyright company="United-Imaging">
+/// Copyright (c) United-Imaging. All rights reserved.
+/// </copyright>
+/// <description>
+/// </description>
+/// ------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using UIH.Mcsf.Core;
+
+namespace UIH.XR.MiniBoot
+{
+    public class MiniApp : Application
+    {
+
+        public MiniApp()
+        {
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MiniBootConfig config = new MiniBootConfig();
+            if (config.SetConfig(e.Args))
+            {
+                MiniBootstrapper boot = new MiniBootstrapper(config);
+                boot.Run();
+            }
+            else
+            {
+                throw new Exception("启动参数无效");
+            }
+        }
+    }
+}
---
+++ Trunk/IAS/CommonFeature/Core.Net/UIH.XR.MiniBoot/MiniBootConfig.cs
@@ -0,0 +1,53 @@
+﻿/// ------------------------------------------------------------------------------
+/// <copyright company="United-Imaging">
+/// Copyright (c) United-Imaging. All rights reserved.
+/// </copyright>
+/// <description>
+/// </description>
+/// ------------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UIH.XR.MiniBoot
+{
+    public class MiniBootConfig
+    {
+        public string ViewName { get; set; }
+
+        public double WindowWidth { get; set; }
+
+        public double WindowHeight { get; set; }
+
+        public List<string> AssemblyList { get; set; }
+
+        public MiniBootConfig()
+        {
+            AssemblyList = new List<string>();
+        }
+
+        public bool SetConfig(string[] args)
+        {
+            if (args.Length < 4)
+                return false;
+
+            try
+            {
+                ViewName = args[0];
+                WindowWidth = double.Parse(args[1]);
+                WindowHeight = double.Parse(args[2]);
+                for (int i = 3; i < args.Length; i++)
+                {
+                    AssemblyList.Add(args[i]);
+                }
+                return true;
+            }
+            catch { }
+
+            return false;
+        }
+    }
+}
